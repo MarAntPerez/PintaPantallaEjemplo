9d215332605858061218193931239a2d3268a056
Marco <maphy1707@gmail.com>
2022-01-14 18:15:05 -0600

    Ejercicio Printf

---
+++ MiNombre.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	printf("OO   OO");
+	printf("\nOOO OOO");
+	printf("\nOOOOOOO");
+	printf("\nOO   OO");
+	printf("\nOO   OO");
+	printf("\n ");
+	
+	printf("\nOOOOOOO");
+	printf("\nOO   OO");
+	printf("\nOOOOOOO");
+	printf("\nOO   OO");
+	printf("\nOO   OO");
+	printf("\n ");
+	
+	printf("\nOOOOOOO");
+	printf("\nOO   OO");
+	printf("\nOOOOOOO");
+	printf("\nOO  OO ");
+	printf("\nOO   OO");
+	printf("\n ");
+	
+	printf("\nOOOOOOO");
+	printf("\nOO     ");
+	printf("\nOO     ");
+	printf("\nOO     ");
+	printf("\nOOOOOOO");
+	printf("\n ");
+	
+	printf("\nOOOOOOO");
+	printf("\nOO   OO");
+	printf("\nOO   OO");
+	printf("\nOO   OO");
+	printf("\nOOOOOOO");
+
+	
+	return 0;
+}
